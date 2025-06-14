bc2061e300da7087155f7b60ef69e9cb14c5b918
Kohei Wada <program3152019@gmail.com>
2025-06-14 16:03:45 +0900

    fix: set default value for tags array in blog schema
    Ensured the tags field in the blog schema defaults to an empty array if not
    provided. This prevents validation errors when tags are omitted in content
    entries.

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Kohei Wada';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'ようこそ、私の個人的なウェブサイトへ。ここでは、私の技術的な情報やプロジェクト、そして私の個人的な興味について紹介しています。';
 export const GITHUB_URL = 'https://github.com/Kohei-Wada';
 export const GOOGLE_FORMS_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdM92biXp5IHHCE_-_jk1EQgP_ULQ3mGIPsfJVlPayWfK7pSA/viewform?embedded=true"
